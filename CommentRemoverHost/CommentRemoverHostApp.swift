65b49cf88a9dee905b75aa747615c22ba5490c7d
Will McGinty <mcgintw@gmail.com>
2021-04-15 19:23:20 -0500

    Initial Commit

---
+++ CommentRemoverHost/CommentRemoverHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentRemoverHostApp.swift
+//  CommentRemoverHost
+//
+//  Created by Will McGinty on 4/15/21.
+//
+
+import SwiftUI
+
+@main
+struct CommentRemoverHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommentRemoverHost/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CommentRemoverHost
+//
+//  Created by Will McGinty on 4/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
